af715e46a567cdf88eafb166252b563d5bc4afc3
Ezeani Christabel <ezeanichristabel9@gmail.com>
2024-10-31 13:51:26 +0100

    week-3 commit

---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Documents\c.ezeaniCSC101\week-3\practice_2\target\debug\practice_2.exe: C:\Users\USER\Documents\c.ezeaniCSC101\week-3\practice_2\src\main.rs
---
+++ week-3/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Documents\c.ezeaniCSC101\week-3\practice_3\target\debug\deps\practice_3.exe: src/main.rs
+
+C:\Users\USER\Documents\c.ezeaniCSC101\week-3\practice_3\target\debug\deps\practice_3.d: src/main.rs
+
+src/main.rs:
